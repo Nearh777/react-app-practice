47ac6b4f14a01dc6b8cec76f858e24f2cce7b532
Nearh777 <nearh777@gmail.com>
2024-04-04 23:58:31 +0300

    create Profile components

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,32 @@
+
+import { Profile } from './Profile';
+import users from './user.json';
+
+
 export const App = () => {
   return (
     <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
+    
+      // style={{
+      //   height: '100vh',
+      //   display: 'flex',
+      //   justifyContent: 'center',
+      //   alignItems: 'center',
+      //   fontSize: 40,
+      //   color: '#010101'
+      // }}
     >
-      React homework template
+      <Profile 
+        username={users.username}
+        tag={users.tag}
+        location={users.location}
+        avatar={users.avatar}
+        followers={users.stats.followers}
+        views={users.stats.views}
+        likes={users.stats.likes}
+
+      />
     </div>
   );
 };
+
