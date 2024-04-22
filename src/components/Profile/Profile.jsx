7a0e7d59717db04fa35f3a0c8865e53515bbf87d
Nearh777 <nearh777@gmail.com>
2024-04-22 21:32:00 +0300

    profile fix

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,16 +1,16 @@
 import PropTypes from 'prop-types';
 // import css from './Profile.module.css';
-import {
-  ProfileContainer,
-  ProfileImage,
-  ProfileInfo,
-  ProfileName,
-  ProfileTag,
-  ProfileLocation,
-  ProfileStats,
-  ProfileStatsLabel,
-  ProfileStatsQuantity,
-} from './Profile.styled';
+// import {
+//   ProfileContainer,
+//   ProfileImage,
+//   ProfileInfo,
+//   ProfileName,
+//   ProfileTag,
+//   ProfileLocation,
+//   ProfileStats,
+//   ProfileStatsLabel,
+//   ProfileStatsQuantity,
+// } from './Profile.styled';
 
 export function Profile({
   userName,
@@ -22,25 +22,55 @@ export function Profile({
   likes,
 }) {
   return (
-    <ProfileContainer>
-      <ProfileInfo>
-        <ProfileImage src={avatar} alt={userName} />
-        <ProfileName>{userName}</ProfileName>
-        <ProfileTag>@{tag}</ProfileTag>
-        <ProfileLocation>{location}</ProfileLocation>
-      </ProfileInfo>
+    // <ProfileContainer>
+    //   <ProfileInfo>
+    //     <ProfileImage src={avatar} alt={userName} />
+    //     <ProfileName>{userName}</ProfileName>
+    //     <ProfileTag>@{tag}</ProfileTag>
+    //     <ProfileLocation>{location}</ProfileLocation>
+    //   </ProfileInfo>
 
-      <ProfileStats>
-        <ProfileStatsLabel>Followers: </ProfileStatsLabel>
-        <ProfileStatsQuantity>{followers}</ProfileStatsQuantity>
+    //   <ProfileStats>
+    //     <ProfileStatsLabel>Followers: </ProfileStatsLabel>
+    //     <ProfileStatsQuantity>{followers}</ProfileStatsQuantity>
 
-        <ProfileStatsLabel>Views: </ProfileStatsLabel>
-        <ProfileStatsQuantity>{views}</ProfileStatsQuantity>
+    //     <ProfileStatsLabel>Views: </ProfileStatsLabel>
+    //     <ProfileStatsQuantity>{views}</ProfileStatsQuantity>
 
-        <ProfileStatsLabel>Likes: </ProfileStatsLabel>
-        <ProfileStatsQuantity>{likes}</ProfileStatsQuantity>
-      </ProfileStats>
-    </ProfileContainer>
+    //     <ProfileStatsLabel>Likes: </ProfileStatsLabel>
+    //     <ProfileStatsQuantity>{likes}</ProfileStatsQuantity>
+    //   </ProfileStats>
+    // </ProfileContainer>
+
+<div className="profile">
+  <div className="description">
+    <img
+      src={avatar}
+      alt={userName}
+      class="avatar"
+    />
+    <p className="name">{userName}</p>
+    <p className="tag">@{tag}</p>
+    <p className="location">{location}</p>
+  </div>
+
+  <ul className="stats">
+    <li>
+      <span className="label">Followers</span>
+      <span className="quantity">{followers}</span>
+    </li>
+    <li>
+      <span className="label">Views</span>
+      <span className="quantity">{views}</span>
+    </li>
+    <li>
+      <span className="label">Likes</span>
+      <span className="quantity">{likes}</span>
+    </li>
+  </ul>
+</div>
+
+
   );
 }
 
